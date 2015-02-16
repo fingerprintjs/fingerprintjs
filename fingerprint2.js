e151ae31fe45368481daf46d0e080308aff380ed
Valentin Vasilyev <valentin.vasilyev@outlook.com>
2015-02-16 12:05:47 +0300

    Add test doc + basic hashing

--- fingerprint2.js
+++ fingerprint2.js
@@ -24,15 +24,20 @@
 })("Fingerprint2", this, function() {
   "use strict";
   var Fingerprint2 = function(options) {
-    var nativeForEach, nativeMap;
-    nativeForEach = Array.prototype.forEach;
-    nativeMap = Array.prototype.map;
-
-    this.each = function (obj, iterator, context) {
+    this.options = options;
+    this.nativeForEach = Array.prototype.forEach;
+    this.nativeMap = Array.prototype.map;
+  };
+  Fingerprint2.prototype = {
+    get: function(){
+      return this.hashString(navigator.userAgent);
+    },
+    
+    each: function (obj, iterator, context) {
       if (obj === null) {
         return;
       }
-      if (nativeForEach && obj.forEach === nativeForEach) {
+      if (this.nativeForEach && obj.forEach === this.nativeForEach) {
         obj.forEach(iterator, context);
       } else if (obj.length === +obj.length) {
         for (var i = 0, l = obj.length; i < l; i++) {
@@ -45,18 +50,63 @@
           }
         }
       }
-    };
+    },
 
-    this.map = function(obj, iterator, context) {
+    map: function(obj, iterator, context) {
       var results = [];
       // Not using strict equality so that this acts as a
       // shortcut to checking for `null` and `undefined`.
       if (obj == null) { return results; }
-      if (nativeMap && obj.map === nativeMap) { return obj.map(iterator, context); }
+      if (this.nativeMap && obj.map === this.nativeMap) { return obj.map(iterator, context); }
       this.each(obj, function(value, index, list) {
         results[results.length] = iterator.call(context, value, index, list);
       });
       return results;
-    };
+    },
+
+    mul32: function(m, n) {
+      var nlo = n & 0xffff;
+      var nhi = n - nlo;
+      return ((nhi * m | 0) + (nlo * m | 0)) | 0;
+    },
+
+    hashString: function(data, len, seed) {
+      var c1 = 0xcc9e2d51, c2 = 0x1b873593;
+
+      var h1 = seed;
+      var roundedEnd = len & ~0x1;
+
+      for (var i = 0; i < roundedEnd; i += 2) {
+        var k1 = data.charCodeAt(i) | (data.charCodeAt(i + 1) << 16);
+
+        k1 = this.mul32(k1, c1);
+        k1 = ((k1 & 0x1ffff) << 15) | (k1 >>> 17);  // ROTL32(k1,15);
+        k1 = this.mul32(k1, c2);
+
+        h1 ^= k1;
+        h1 = ((h1 & 0x7ffff) << 13) | (h1 >>> 19);  // ROTL32(h1,13);
+        h1 = (h1 * 5 + 0xe6546b64) | 0;
+      }
+
+      if((len % 2) == 1) {
+        k1 = data.charCodeAt(roundedEnd);
+        k1 = this.mul32(k1, c1);
+        k1 = ((k1 & 0x1ffff) << 15) | (k1 >>> 17);  // ROTL32(k1,15);
+        k1 = this.mul32(k1, c2);
+        h1 ^= k1;
+      }
+
+      // finalization
+      h1 ^= (len << 1);
+
+      // fmix(h1);
+      h1 ^= h1 >>> 16;
+      h1  = this.mul32(h1, 0x85ebca6b);
+      h1 ^= h1 >>> 13;
+      h1  = this.mul32(h1, 0xc2b2ae35);
+      h1 ^= h1 >>> 16;
+      return h1;
+    }
   };
+  return Fingerprint2;
 });
