df7cdc1501bcd3052cebefc0a409bd1030af7443
Valentin Vasilyev <valentin.vasilyev@outlook.com>
2015-03-03 12:46:34 +0300

    [gh-pages] Add fingerprintjs2 file

---
+++ fingerprintjs2.js
@@ -0,0 +1,503 @@
+/*
+* Fingerprintjs2 0.0.1 - Modern & flexible browser fingerprint library v2
+* https://github.com/Valve/fingerprintjs2
+* Copyright (c) 2015 Valentin Vasilyev (valentin.vasilyev@outlook.com)
+* Licensed under the MIT (http://www.opensource.org/licenses/mit-license.php) license.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+* ARE DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+* THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+(function (name, context, definition) {
+  "use strict";
+  if (typeof module !== "undefined" && module.exports) { module.exports = definition(); }
+  else if (typeof define === "function" && define.amd) { define(definition); }
+  else { context[name] = definition(); }
+})("Fingerprint2", this, function() {
+  "use strict";
+  var DEBUG = true;
+  var Fingerprint2 = function(options) {
+    // TODO: merge options
+    this.options = {
+      swfContainerId: "fingerprintjs2",
+      swfPath: "flash/compiled/FontList.swf"
+    };
+    this.nativeForEach = Array.prototype.forEach;
+    this.nativeMap = Array.prototype.map;
+  };
+  Fingerprint2.prototype = {
+    log: function(msg){
+      if(window.console){
+        console.log(msg);
+      }
+    },
+    get: function(done){
+      var keys = [];
+      keys = this.userAgentKey(keys);
+      keys = this.languageKey(keys);
+      keys = this.colorDepthKey(keys);
+      keys = this.screenResolutionKey(keys);
+      keys = this.timezoneOffsetKey(keys);
+      keys = this.sessionStorageKey(keys);
+      keys = this.localStorageKey(keys);
+      keys = this.indexedDbKey(keys);
+      keys = this.addBehaviorKey(keys);
+      keys = this.openDatabaseKey(keys);
+      keys = this.cpuClassKey(keys);
+      keys = this.platformKey(keys);
+      keys = this.doNotTrackKey(keys);
+      keys = this.canvasKey(keys);
+      var that = this;
+      this.flashFontsKey(keys, function(newKeys){
+        var murmur = that.x64hash128(newKeys.join("~~~"), 31);
+        return done(murmur);
+      });
+    },
+
+    userAgentKey: function(keys) {
+      if(!this.options.excludeUserAgent) {
+        keys.push(navigator.userAgent);
+      }
+      return keys;
+    },
+    languageKey: function(keys) {
+      if(!this.options.excludeLanguage) {
+        keys.push(navigator.language);
+      }
+      return keys;
+    },
+    colorDepthKey: function(keys) {
+      if(!this.options.excludeColorDepth) {
+        keys.push(screen.colorDepth);
+      }
+      return keys;
+    },
+    screenResolutionKey: function(keys) {
+      if(!this.options.excludeScreenResolution) {
+        var resolution = this.getScreenResolution();
+        if (typeof resolution !== "undefined"){ // headless browsers, such as phantomjs
+          keys.push(resolution.join("x"));
+        }
+      }
+      return keys;
+    },
+    getScreenResolution: function () {
+      var resolution;
+      if(this.options.detectScreenOrientation) {
+        resolution = (screen.height > screen.width) ? [screen.height, screen.width] : [screen.width, screen.height];
+      } else {
+        resolution = [screen.height, screen.width];
+      }
+      return resolution;
+    },
+    timezoneOffsetKey: function(keys) {
+      if(!this.options.excludeTimezoneOffset) {
+        keys.push(new Date().getTimezoneOffset());
+      }
+      return keys;
+    },
+    sessionStorageKey: function(keys) {
+      if(!this.options.excludeSessionStorage && this.hasSessionStorage()) {
+        keys.push("sessionStorageKey");
+      }
+      return keys;
+    },
+    localStorageKey: function(keys) {
+      if(!this.options.excludeSessionStorage && this.hasLocalStorage()) {
+        keys.push("localStorageKey");
+      }
+      return keys;
+    },
+    indexedDbKey: function(keys) {
+      if(!this.options.excludeIndexedDB && this.hasIndexedDB()) {
+        keys.push("indexedDbKey");
+      }
+      return keys;
+    },
+    addBehaviorKey: function(keys) {
+      //body might not be defined at this point or removed programmatically
+      if(document.body && !this.options.excludeAddBehavior && document.body.addBehavior) {
+        keys.push("addBehaviorKey");
+      }
+      return keys;
+    },
+    openDatabaseKey: function(keys) {
+      if(!this.options.excludeOpenDatabase && window.openDatabase) {
+        keys.push("openDatabase");
+      }
+      return keys;
+    },
+    cpuClassKey: function(keys) {
+      if(!this.options.excludeCpuClass) {
+        keys.push(this.getNavigatorCpuClass());
+      }
+      return keys;
+    },
+    platformKey: function(keys) {
+      if(!this.options.excludePlatform) {
+        keys.push(this.getNavigatorPlatform());
+      }
+      return keys;
+    },
+    doNotTrackKey: function(keys) {
+      if(!this.options.excludeDoNotTrack) {
+        keys.push(this.getDoNotTrack());
+      }
+      return keys;
+    },
+    canvasKey: function(keys) {
+      if(!this.options.excludeCanvas && this.isCanvasSupported()) {
+        keys.push(this.getCanvasFp());
+      }
+      return keys;
+    },
+    // flash fonts (will increase fingerprinting time 20X to ~ 130-150ms)
+    flashFontsKey: function(keys, done) {
+      if(this.options.excludeFlashFonts) {
+        if(DEBUG){
+          this.log("Skipping flash fonts detection per excludeFlashFonts configuration option");
+        }
+        return done(keys);
+      }
+      // we do flash if swfobject is loaded
+      if(!this.hasSwfObjectLoaded()){
+        if(DEBUG){
+          this.log("Swfobject is not detected, Flash fonts enumeration is skipped");
+        }
+        return done(keys);
+      }
+      if(!this.hasMinFlashInstalled()){
+        if(DEBUG){
+          this.log("Flash is not installed, skipping Flash fonts enumeration");
+        }
+        return done(keys);
+      }
+      if(typeof this.options.swfPath === "undefined"){
+        if(DEBUG){
+          this.log("To use Flash fonts detection, you must pass the swfPath option, skipping Flash fonts enumeration");
+        }
+        return done(keys);
+      }
+      this.loadSwfAndDetectFonts(function(fonts){
+        keys.push(fonts.join(";"));
+        done(keys);
+      });
+    },
+    hasSessionStorage: function () {
+      try {
+        return !!window.sessionStorage;
+      } catch(e) {
+        return true; // SecurityError when referencing it means it exists
+      }
+    },
+    // https://bugzilla.mozilla.org/show_bug.cgi?id=781447
+    hasLocalStorage: function () {
+      try {
+        return !!window.localStorage;
+      } catch(e) {
+        return true; // SecurityError when referencing it means it exists
+      }
+    },
+    hasIndexedDB: function (){
+      return !!window.indexedDB;
+    },
+    getNavigatorCpuClass: function () {
+      if(navigator.cpuClass){
+        return "navigatorCpuClass: " + navigator.cpuClass;
+      } else {
+        return "navigatorCpuClass: unknown";
+      }
+    },
+    getNavigatorPlatform: function () {
+      if(navigator.platform) {
+        return "navigatorPlatform: " + navigator.platform;
+      } else {
+        return "navigatorPlatform: unknown";
+      }
+    },
+    getDoNotTrack: function () {
+      if(navigator.doNotTrack) {
+        return "doNotTrack: " + navigator.doNotTrack;
+      } else {
+        return "doNotTrack: unknown";
+      }
+    },
+    getCanvasFp: function() {
+      // Very simple now, need to make it more complex (geo shapes etc)
+      var canvas = document.createElement("canvas");
+      var ctx = canvas.getContext("2d");
+      // https://www.browserleaks.com/canvas#how-does-it-work
+      var txt = "Cwm fjordbank glyphs vext quiz, https://github.com/valve ὠ";
+      ctx.textBaseline = "top";
+      ctx.font = "70px 'Arial'";
+      ctx.textBaseline = "alphabetic";
+      ctx.fillStyle = "#f60";
+      ctx.fillRect(125, 1, 62, 20);
+      ctx.fillStyle = "#069";
+      ctx.fillText(txt, 2, 15);
+      ctx.fillStyle = "rgba(102, 204, 0, 0.7)";
+      ctx.fillText(txt, 4, 17);
+      return canvas.toDataURL();
+    },
+    isCanvasSupported: function () {
+      var elem = document.createElement("canvas");
+      return !!(elem.getContext && elem.getContext("2d"));
+    },
+    hasSwfObjectLoaded: function(){
+      return typeof window.swfobject !== "undefined";
+    },
+    hasMinFlashInstalled: function () {
+      return swfobject.hasFlashPlayerVersion("9.0.0");
+    },
+    addFlashDivNode: function() {
+      var node = document.createElement("div");
+      node.setAttribute("id", this.options.swfContainerId);
+      document.body.appendChild(node);
+    },
+    loadSwfAndDetectFonts: function(done) {
+      var hiddenCallback = "___fp_swf_loaded";
+      window[hiddenCallback] = function(fonts) {
+        done(fonts);
+      };
+      var id = this.options.swfContainerId;
+      this.addFlashDivNode();
+      var flashvars = { onReady: hiddenCallback};
+      var flashparams = { allowScriptAccess: "always", menu: "false" };
+      swfobject.embedSWF(this.options.swfPath, id, "1", "1", "9.0.0", false, flashvars, flashparams, {});
+    },
+    each: function (obj, iterator, context) {
+      if (obj === null) {
+        return;
+      }
+      if (this.nativeForEach && obj.forEach === this.nativeForEach) {
+        obj.forEach(iterator, context);
+      } else if (obj.length === +obj.length) {
+        for (var i = 0, l = obj.length; i < l; i++) {
+          if (iterator.call(context, obj[i], i, obj) === {}) { return; }
+        }
+      } else {
+        for (var key in obj) {
+          if (obj.hasOwnProperty(key)) {
+            if (iterator.call(context, obj[key], key, obj) === {}) { return; }
+          }
+        }
+      }
+    },
+
+    map: function(obj, iterator, context) {
+      var results = [];
+      // Not using strict equality so that this acts as a
+      // shortcut to checking for `null` and `undefined`.
+      if (obj == null) { return results; }
+      if (this.nativeMap && obj.map === this.nativeMap) { return obj.map(iterator, context); }
+      this.each(obj, function(value, index, list) {
+        results[results.length] = iterator.call(context, value, index, list);
+      });
+      return results;
+    },
+
+    /// MurmurHash3 related functions
+
+    //
+    // Given two 64bit ints (as an array of two 32bit ints) returns the two
+    // added together as a 64bit int (as an array of two 32bit ints).
+    //
+    x64Add: function(m, n) {
+      m = [m[0] >>> 16, m[0] & 0xffff, m[1] >>> 16, m[1] & 0xffff];
+      n = [n[0] >>> 16, n[0] & 0xffff, n[1] >>> 16, n[1] & 0xffff];
+      var o = [0, 0, 0, 0];
+      o[3] += m[3] + n[3];
+      o[2] += o[3] >>> 16;
+      o[3] &= 0xffff;
+      o[2] += m[2] + n[2];
+      o[1] += o[2] >>> 16;
+      o[2] &= 0xffff;
+      o[1] += m[1] + n[1];
+      o[0] += o[1] >>> 16;
+      o[1] &= 0xffff;
+      o[0] += m[0] + n[0];
+      o[0] &= 0xffff;
+      return [(o[0] << 16) | o[1], (o[2] << 16) | o[3]];
+    },
+
+    //
+    // Given two 64bit ints (as an array of two 32bit ints) returns the two
+    // multiplied together as a 64bit int (as an array of two 32bit ints).
+    //
+    x64Multiply: function(m, n) {
+      m = [m[0] >>> 16, m[0] & 0xffff, m[1] >>> 16, m[1] & 0xffff];
+      n = [n[0] >>> 16, n[0] & 0xffff, n[1] >>> 16, n[1] & 0xffff];
+      var o = [0, 0, 0, 0];
+      o[3] += m[3] * n[3];
+      o[2] += o[3] >>> 16;
+      o[3] &= 0xffff;
+      o[2] += m[2] * n[3];
+      o[1] += o[2] >>> 16;
+      o[2] &= 0xffff;
+      o[2] += m[3] * n[2];
+      o[1] += o[2] >>> 16;
+      o[2] &= 0xffff;
+      o[1] += m[1] * n[3];
+      o[0] += o[1] >>> 16;
+      o[1] &= 0xffff;
+      o[1] += m[2] * n[2];
+      o[0] += o[1] >>> 16;
+      o[1] &= 0xffff;
+      o[1] += m[3] * n[1];
+      o[0] += o[1] >>> 16;
+      o[1] &= 0xffff;
+      o[0] += (m[0] * n[3]) + (m[1] * n[2]) + (m[2] * n[1]) + (m[3] * n[0]);
+      o[0] &= 0xffff;
+      return [(o[0] << 16) | o[1], (o[2] << 16) | o[3]];
+    },
+    //
+    // Given a 64bit int (as an array of two 32bit ints) and an int
+    // representing a number of bit positions, returns the 64bit int (as an
+    // array of two 32bit ints) rotated left by that number of positions.
+    //
+    x64Rotl: function(m, n) {
+      n %= 64;
+      if (n === 32) {
+        return [m[1], m[0]];
+      }
+      else if (n < 32) {
+        return [(m[0] << n) | (m[1] >>> (32 - n)), (m[1] << n) | (m[0] >>> (32 - n))];
+      }
+      else {
+        n -= 32;
+        return [(m[1] << n) | (m[0] >>> (32 - n)), (m[0] << n) | (m[1] >>> (32 - n))];
+      }
+    },
+    //
+    // Given a 64bit int (as an array of two 32bit ints) and an int
+    // representing a number of bit positions, returns the 64bit int (as an
+    // array of two 32bit ints) shifted left by that number of positions.
+    //
+    x64LeftShift: function(m, n) {
+      n %= 64;
+      if (n === 0) {
+        return m;
+      }
+      else if (n < 32) {
+        return [(m[0] << n) | (m[1] >>> (32 - n)), m[1] << n];
+      }
+      else {
+        return [m[1] << (n - 32), 0];
+      }
+    },
+    //
+    // Given two 64bit ints (as an array of two 32bit ints) returns the two
+    // xored together as a 64bit int (as an array of two 32bit ints).
+    //
+    x64Xor: function(m, n) {
+      return [m[0] ^ n[0], m[1] ^ n[1]];
+    },
+    //
+    // Given a block, returns murmurHash3's final x64 mix of that block.
+    // (`[0, h[0] >>> 1]` is a 33 bit unsigned right shift. This is the
+    // only place where we need to right shift 64bit ints.)
+    //
+    x64Fmix: function(h) {
+      h = this.x64Xor(h, [0, h[0] >>> 1]);
+      h = this.x64Multiply(h, [0xff51afd7, 0xed558ccd]);
+      h = this.x64Xor(h, [0, h[0] >>> 1]);
+      h = this.x64Multiply(h, [0xc4ceb9fe, 0x1a85ec53]);
+      h = this.x64Xor(h, [0, h[0] >>> 1]);
+      return h;
+    },
+
+    //
+    // Given a string and an optional seed as an int, returns a 128 bit
+    // hash using the x64 flavor of MurmurHash3, as an unsigned hex.
+    //
+    x64hash128: function (key, seed) {
+      key = key || "";
+      seed = seed || 0;
+      var remainder = key.length % 16;
+      var bytes = key.length - remainder;
+      var h1 = [0, seed];
+      var h2 = [0, seed];
+      var k1 = [0, 0];
+      var k2 = [0, 0];
+      var c1 = [0x87c37b91, 0x114253d5];
+      var c2 = [0x4cf5ad43, 0x2745937f];
+      for (var i = 0; i < bytes; i = i + 16) {
+        k1 = [((key.charCodeAt(i + 4) & 0xff)) | ((key.charCodeAt(i + 5) & 0xff) << 8) | ((key.charCodeAt(i + 6) & 0xff) << 16) | ((key.charCodeAt(i + 7) & 0xff) << 24), ((key.charCodeAt(i) & 0xff)) | ((key.charCodeAt(i + 1) & 0xff) << 8) | ((key.charCodeAt(i + 2) & 0xff) << 16) | ((key.charCodeAt(i + 3) & 0xff) << 24)];
+        k2 = [((key.charCodeAt(i + 12) & 0xff)) | ((key.charCodeAt(i + 13) & 0xff) << 8) | ((key.charCodeAt(i + 14) & 0xff) << 16) | ((key.charCodeAt(i + 15) & 0xff) << 24), ((key.charCodeAt(i + 8) & 0xff)) | ((key.charCodeAt(i + 9) & 0xff) << 8) | ((key.charCodeAt(i + 10) & 0xff) << 16) | ((key.charCodeAt(i + 11) & 0xff) << 24)];
+        k1 = this.x64Multiply(k1, c1);
+        k1 = this.x64Rotl(k1, 31);
+        k1 = this.x64Multiply(k1, c2);
+        h1 = this.x64Xor(h1, k1);
+        h1 = this.x64Rotl(h1, 27);
+        h1 = this.x64Add(h1, h2);
+        h1 = this.x64Add(this.x64Multiply(h1, [0, 5]), [0, 0x52dce729]);
+        k2 = this.x64Multiply(k2, c2);
+        k2 = this.x64Rotl(k2, 33);
+        k2 = this.x64Multiply(k2, c1);
+        h2 = this.x64Xor(h2, k2);
+        h2 = this.x64Rotl(h2, 31);
+        h2 = this.x64Add(h2, h1);
+        h2 = this.x64Add(this.x64Multiply(h2, [0, 5]), [0, 0x38495ab5]);
+      }
+      k1 = [0, 0];
+      k2 = [0, 0];
+      switch(remainder) {
+        case 15:
+          k2 = this.x64Xor(k2, this.x64LeftShift([0, key.charCodeAt(i + 14)], 48));
+        case 14:
+          k2 = this.x64Xor(k2, this.x64LeftShift([0, key.charCodeAt(i + 13)], 40));
+        case 13:
+          k2 = this.x64Xor(k2, this.x64LeftShift([0, key.charCodeAt(i + 12)], 32));
+        case 12:
+          k2 = this.x64Xor(k2, this.x64LeftShift([0, key.charCodeAt(i + 11)], 24));
+        case 11:
+          k2 = this.x64Xor(k2, this.x64LeftShift([0, key.charCodeAt(i + 10)], 16));
+        case 10:
+          k2 = this.x64Xor(k2, this.x64LeftShift([0, key.charCodeAt(i + 9)], 8));
+        case 9:
+          k2 = this.x64Xor(k2, [0, key.charCodeAt(i + 8)]);
+          k2 = this.x64Multiply(k2, c2);
+          k2 = this.x64Rotl(k2, 33);
+          k2 = this.x64Multiply(k2, c1);
+          h2 = this.x64Xor(h2, k2);
+        case 8:
+          k1 = this.x64Xor(k1, this.x64LeftShift([0, key.charCodeAt(i + 7)], 56));
+        case 7:
+          k1 = this.x64Xor(k1, this.x64LeftShift([0, key.charCodeAt(i + 6)], 48));
+        case 6:
+          k1 = this.x64Xor(k1, this.x64LeftShift([0, key.charCodeAt(i + 5)], 40));
+        case 5:
+          k1 = this.x64Xor(k1, this.x64LeftShift([0, key.charCodeAt(i + 4)], 32));
+        case 4:
+          k1 = this.x64Xor(k1, this.x64LeftShift([0, key.charCodeAt(i + 3)], 24));
+        case 3:
+          k1 = this.x64Xor(k1, this.x64LeftShift([0, key.charCodeAt(i + 2)], 16));
+        case 2:
+          k1 = this.x64Xor(k1, this.x64LeftShift([0, key.charCodeAt(i + 1)], 8));
+        case 1:
+          k1 = this.x64Xor(k1, [0, key.charCodeAt(i)]);
+          k1 = this.x64Multiply(k1, c1);
+          k1 = this.x64Rotl(k1, 31);
+          k1 = this.x64Multiply(k1, c2);
+          h1 = this.x64Xor(h1, k1);
+      }
+      h1 = this.x64Xor(h1, [0, key.length]);
+      h2 = this.x64Xor(h2, [0, key.length]);
+      h1 = this.x64Add(h1, h2);
+      h2 = this.x64Add(h2, h1);
+      h1 = this.x64Fmix(h1);
+      h2 = this.x64Fmix(h2);
+      h1 = this.x64Add(h1, h2);
+      h2 = this.x64Add(h2, h1);
+      return ("00000000" + (h1[0] >>> 0).toString(16)).slice(-8) + ("00000000" + (h1[1] >>> 0).toString(16)).slice(-8) + ("00000000" + (h2[0] >>> 0).toString(16)).slice(-8) + ("00000000" + (h2[1] >>> 0).toString(16)).slice(-8);
+    }
+  };
+  return Fingerprint2;
+});
