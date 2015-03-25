cbfe034e0468d343abd9844ff655649889da8930
Valentin Vasilyev <valentin.vasilyev@outlook.com>
2015-03-25 16:16:45 +0300

    Implement webgl fingerprinting

--- fingerprint2.js
+++ fingerprint2.js
@@ -1,5 +1,5 @@
 /*
-* Fingerprintjs2 0.0.1 - Modern & flexible browser fingerprint library v2
+* Fingerprintjs2 0.0.5 - Modern & flexible browser fingerprint library v2
 * https://github.com/Valve/fingerprintjs2
 * Copyright (c) 2015 Valentin Vasilyev (valentin.vasilyev@outlook.com)
 * Licensed under the MIT (http://www.opensource.org/licenses/mit-license.php) license.
@@ -63,6 +63,7 @@
       keys = this.platformKey(keys);
       keys = this.doNotTrackKey(keys);
       keys = this.canvasKey(keys);
+      keys = this.webglKey(keys);
       var that = this;
       this.fontsKey(keys, function(newKeys){
         var murmur = that.x64hash128(newKeys.join("~~~"), 31);
@@ -164,6 +165,12 @@
     canvasKey: function(keys) {
       if(!this.options.excludeCanvas && this.isCanvasSupported()) {
         keys.push(this.getCanvasFp());
+      }
+      return keys;
+    },
+    webglKey: function(keys) {
+      if(!this.options.excludeWebGL && this.isCanvasSupported()) {
+        keys.push(this.getWebglFp());
       }
       return keys;
     },
@@ -315,6 +322,74 @@
       ctx.fillText(txt, 4, 17);
       return canvas.toDataURL();
     },
+
+    getWebglFp: function() {
+      var fa2s = function(fa) {
+        return "[" + fa[0] + ", " + fa[1] + "]"
+      };
+      var maxAnisotropy = function(gl) {
+        var anisotropy, ext = gl.getExtension("EXT_texture_filter_anisotropic") || gl.getExtension("WEBKIT_EXT_texture_filter_anisotropic") || gl.getExtension("MOZ_EXT_texture_filter_anisotropic");
+        return ext ? (anisotropy = gl.getParameter(ext.MAX_TEXTURE_MAX_ANISOTROPY_EXT), 0 === anisotropy && (anisotropy = 2), anisotropy) : null
+      };
+      var gl = this.getWebglCanvas();
+      if(!gl) return null;
+      // WebGL fingerprinting is a combination of techniques, found in MaxMind antifraud script & Augur fingerprinting.
+      // First it draws a gradient object with shaders and convers the image to the Base64 string.
+      // Then it enumerates all WebGL extensions & capabilities and appends them to the Base64 string, resulting in a huge WebGL string, potentially very unique on each device
+      // Since iOS supports webgl starting from version 8.1 and 8.1 runs on several graphics chips, the results may be different across ios devices, but we need to verify it.
+      var result = [];
+      var canvas = document.createElement("canvas");
+      var vShaderTemplate = "attribute vec2 attrVertex;varying vec2 varyinTexCoordinate;uniform vec2 uniformOffset;void main(){varyinTexCoordinate=attrVertex+uniformOffset;gl_Position=vec4(attrVertex,0,1);}";
+      var fShaderTemplate = "precision mediump float;varying vec2 varyinTexCoordinate;void main() {gl_FragColor=vec4(varyinTexCoordinate,0,1);}";
+      var vertexPosBuffer = gl.createBuffer();
+      gl.bindBuffer(gl.ARRAY_BUFFER, vertexPosBuffer);
+      var vertices = new Float32Array([-.2, -.9, 0, .4, -.26, 0, 0, .732134444, 0]);
+      gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW), vertexPosBuffer.itemSize = 3, vertexPosBuffer.numItems = 3;
+      var program = gl.createProgram(), vshader = gl.createShader(gl.VERTEX_SHADER);
+      gl.shaderSource(vshader, vShaderTemplate), gl.compileShader(vshader);
+      var fshader = gl.createShader(gl.FRAGMENT_SHADER);
+      gl.shaderSource(fshader, fShaderTemplate);
+      gl.compileShader(fshader);
+      gl.attachShader(program, vshader);
+      gl.attachShader(program, fshader);
+      gl.linkProgram(program);
+      gl.useProgram(program);
+      program.vertexPosAttrib = gl.getAttribLocation(program, "attrVertex");
+      program.offsetUniform = gl.getUniformLocation(program, "uniformOffset");
+      gl.enableVertexAttribArray(program.vertexPosArray);
+      gl.vertexAttribPointer(program.vertexPosAttrib, vertexPosBuffer.itemSize, gl.FLOAT, !1, 0, 0);
+      gl.uniform2f(program.offsetUniform, 1, 1);
+      gl.drawArrays(gl.TRIANGLE_STRIP, 0, vertexPosBuffer.numItems);
+      result.push(canvas.toDataURL());
+      result.push("extensions:" + gl.getSupportedExtensions().join(";"));
+      result.push("webgl aliased line width range:" + fa2s(gl.getParameter(gl.ALIASED_LINE_WIDTH_RANGE)));
+      result.push("webgl aliased point size range:" + fa2s(gl.getParameter(gl.ALIASED_POINT_SIZE_RANGE)));
+      result.push("webgl alpha bits:" + gl.getParameter(gl.ALPHA_BITS));
+      result.push("webgl antialiasing:" + (gl.getContextAttributes().antialias ? "yes" : "no"));
+      result.push("webgl blue bits:" + gl.getParameter(gl.BLUE_BITS));
+      result.push("webgl depth bits:" + gl.getParameter(gl.DEPTH_BITS));
+      result.push("webgl green bits:" + gl.getParameter(gl.GREEN_BITS));
+      result.push("webgl max anisotropy:" + maxAnisotropy(gl));
+      result.push("webgl max combined texture image units:" + gl.getParameter(gl.MAX_COMBINED_TEXTURE_IMAGE_UNITS));
+      result.push("webgl max cube map texture size:" + gl.getParameter(gl.MAX_CUBE_MAP_TEXTURE_SIZE));
+      result.push("webgl max fragment uniform vectors:" + gl.getParameter(gl.MAX_FRAGMENT_UNIFORM_VECTORS));
+      result.push("webgl max render buffer size:" + gl.getParameter(gl.MAX_RENDERBUFFER_SIZE));
+      result.push("webgl max texture image units:" + gl.getParameter(gl.MAX_TEXTURE_IMAGE_UNITS));
+      result.push("webgl max texture size:" + gl.getParameter(gl.MAX_TEXTURE_SIZE));
+      result.push("webgl max varying vectors:" + gl.getParameter(gl.MAX_VARYING_VECTORS));
+      result.push("webgl max vertex attribs:" + gl.getParameter(gl.MAX_VERTEX_ATTRIBS));
+      result.push("webgl max vertex texture image units:" + gl.getParameter(gl.MAX_VERTEX_TEXTURE_IMAGE_UNITS));
+      result.push("webgl max vertex uniform vectors:" + gl.getParameter(gl.MAX_VERTEX_UNIFORM_VECTORS));
+      result.push("webgl max viewport dims:" + fa2s(gl.getParameter(gl.MAX_VIEWPORT_DIMS)));
+      result.push("webgl red bits:" + gl.getParameter(gl.RED_BITS));
+      result.push("webgl renderer:" + gl.getParameter(gl.RENDERER));
+      result.push("webgl shading language version:" + gl.getParameter(gl.SHADING_LANGUAGE_VERSION));
+      result.push("webgl stencil bits:" + gl.getParameter(gl.STENCIL_BITS));
+      result.push("webgl vendor:" + gl.getParameter(gl.VENDOR));
+      result.push("webgl version:" + gl.getParameter(gl.VERSION));
+      //TODO: implement vertex shader & fragment shader precision
+      return result.join("§");
+    },
     isCanvasSupported: function () {
       var elem = document.createElement("canvas");
       return !!(elem.getContext && elem.getContext("2d"));
@@ -340,6 +415,15 @@
       var flashvars = { onReady: hiddenCallback};
       var flashparams = { allowScriptAccess: "always", menu: "false" };
       swfobject.embedSWF(this.options.swfPath, id, "1", "1", "9.0.0", false, flashvars, flashparams, {});
+    },
+    getWebglCanvas: function() {
+      var canvas = document.createElement("canvas");
+      var gl = null;
+      try {
+        gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+      } catch(e) {}
+      if(!gl){gl = null;}
+      return gl;
     },
     each: function (obj, iterator, context) {
       if (obj === null) {
