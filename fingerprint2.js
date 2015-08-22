f00f73845e2f389e6300312536defa0462819d85
Valentin Vasilyev <valentin.vasilyev@outlook.com>
2015-08-22 12:29:46 +0300

    New version

--- fingerprint2.js
+++ fingerprint2.js
@@ -1,5 +1,5 @@
 /*
-* Fingerprintjs2 0.7.1 - Modern & flexible browser fingerprint library v2
+* Fingerprintjs2 0.7.2 - Modern & flexible browser fingerprint library v2
 * https://github.com/Valve/fingerprintjs2
 * Copyright (c) 2015 Valentin Vasilyev (valentin.vasilyev@outlook.com)
 * Licensed under the MIT (http://www.opensource.org/licenses/mit-license.php) license.
@@ -1143,6 +1143,6 @@
       return ("00000000" + (h1[0] >>> 0).toString(16)).slice(-8) + ("00000000" + (h1[1] >>> 0).toString(16)).slice(-8) + ("00000000" + (h2[0] >>> 0).toString(16)).slice(-8) + ("00000000" + (h2[1] >>> 0).toString(16)).slice(-8);
     }
   };
-  Fingerprint2.VERSION = "0.7.1";
+  Fingerprint2.VERSION = "0.7.2";
   return Fingerprint2;
 });
