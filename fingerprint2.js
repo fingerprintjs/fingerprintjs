d6ec14918c1a9a225c5d620524d868d6e1514a9a
Andreas Franzén <andreas@devil.se>
2016-04-22 14:01:24 +0200

    Add excludeIEPlugins option
    In atleast newer versions of Internet Explorer the plugin detection triggers a user confirmation bar as seen in #142.
    I would like a option to make sure that doesn't happen.

--- fingerprint2.js
+++ fingerprint2.js
@@ -438,7 +438,9 @@
     pluginsKey: function(keys) {
       if(!this.options.excludePlugins){
         if(this.isIE()){
-          keys.push({key: "ie_plugins", value: this.getIEPlugins()});
+          if(!this.options.excludeIEPlugins) {
+            keys.push({key: "ie_plugins", value: this.getIEPlugins()});
+          }
         } else {
           keys.push({key: "regular_plugins", value: this.getRegularPlugins()});
         }
