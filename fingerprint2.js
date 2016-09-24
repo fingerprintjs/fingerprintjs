5a69623aa5645e7e98c3deebb39342d23c34736a
Sanasol <sanasol2008@yandex.ru>
2016-09-24 14:08:14 +0300

    Skip browser version, to avoid hash changing after browser update

--- fingerprint2.js
+++ fingerprint2.js
@@ -128,7 +128,52 @@
     },
     // for tests
     getUserAgent: function(){
-      return navigator.userAgent;
+      var userAgent = navigator.userAgent;
+      if(this.options.excludeBrowserVersion) {
+          userAgent = userAgent.replace(this.getBrowserVersion(), "");
+      }
+      return userAgent;
+    },
+    getBrowserVersion: function(){
+      var uagent = navigator.userAgent.toLowerCase(),
+          match = "";
+      var browser = {};
+
+      browser.chrome = /webkit/.test(uagent) && /chrome/.test(uagent) &&
+                         !/edge/.test(uagent);
+
+      browser.firefox = /mozilla/.test(uagent) && /firefox/.test(uagent);
+
+      browser.msie = /msie/.test(uagent) || /trident/.test(uagent) ||
+                         /edge/.test(uagent);
+
+      browser.safari = /safari/.test(uagent) && /applewebkit/.test(uagent) &&
+                         !/chrome/.test(uagent) && !/phantomjs/.test(uagent);
+
+      browser.opr = /mozilla/.test(uagent) && /applewebkit/.test(uagent) &&
+                         /chrome/.test(uagent) && /safari/.test(uagent) &&
+                         /opr/.test(uagent);
+
+      browser.phantomjs = /phantomjs/.test(uagent); // For testing purposes
+
+      browser.version = "";
+
+      for (var x in browser) {
+        if (browser[x]) {
+          match = uagent.match(
+                    new RegExp("(" + (x === "msie" ? "msie|edge" : x) + ")( |\/)([0-9]+(.[0-9]+)?(.[0-9]+)?(.[0-9]+)?)")
+                  );
+
+          if (match) {
+            browser.version = match[3];
+          } else {
+            match = uagent.match(new RegExp("rv:([0-9]+)"));
+            browser.version = match ? match[1] : "";
+          }
+          break;
+        }
+      }
+      return browser.version;
     },
     languageKey: function(keys) {
       if(!this.options.excludeLanguage) {
