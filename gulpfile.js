c3a67a4191aa6aafbfafd0e92fecb3ab2f1612f4
Jonas Haag <jonas@lophus.org>
2018-04-09 13:51:33 +0200

    Lint specs.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,31 @@
-var gulp = require("gulp"),
-    standard = require("gulp-standard"),
-    rename = require("gulp-rename"),
-    uglify = require("gulp-uglify");
+const gulp = require('gulp')
+const standard = require('gulp-standard')
+const rename = require('gulp-rename')
+const uglify = require('gulp-uglify')
 
-gulp.task("lint", function() {
+gulp.task('lint', function () {
   return gulp
-    .src("fingerprint2.js")
+    .src('fingerprint2.js')
     .pipe(standard())
     .pipe(standard.reporter('default', {
       breakOnError: true,
       quiet: true
     }))
-});
+})
 
-gulp.task("minify", function() {
+gulp.task('minify', function () {
   return gulp
-      .src("fingerprint2.js")
-      .pipe(rename({suffix: ".min"}))
+      .src('fingerprint2.js')
+      .pipe(rename({suffix: '.min'}))
       .pipe(uglify({
-          compress: {
-            global_defs: {}
-          },
-          output: {
-              ascii_only: true
-          }
+        compress: {
+          global_defs: {}
+        },
+        output: {
+          ascii_only: true
+        }
       }))
-      .pipe(gulp.dest("dist/"));
-});
+      .pipe(gulp.dest('dist/'))
+})
 
-
-gulp.task("default", ["lint", "minify"], function() {});
+gulp.task('default', ['lint', 'minify'], function () {})
