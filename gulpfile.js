e71fb90f5975485b24d808ace3e7a6c8288f9c7b
Jonas Haag <jonas@lophus.org>
2018-10-11 18:05:17 +0200

    Re-release 1.8.2 as 1.8.5

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,9 @@
 const gulp = require('gulp')
-const standard = require('gulp-standard')
 const rename = require('gulp-rename')
 const uglify = require('gulp-uglify')
-
-gulp.task('lint', function () {
-  return gulp
-    .src('fingerprint2.js')
-    .pipe(standard())
-    .pipe(standard.reporter('default', {
-      breakOnError: true,
-      quiet: true
-    }))
-})
+const license = require('gulp-header-license')
+const fs = require('fs')
+const year = (new Date()).getFullYear()
 
 gulp.task('minify', function () {
   return gulp
@@ -24,10 +16,11 @@ gulp.task('minify', function () {
         output: {
           ascii_only: true
         }
-       }).on('error', function(e) {
-         console.log(e);
+      }).on('error', function (e) {
+        console.log(e)
       }))
+      .pipe(license(fs.readFileSync('license.txt', 'utf8'), {YEAR: year}, 0.9))
       .pipe(gulp.dest('dist/'))
 })
 
-gulp.task('default', ['lint', 'minify'], function () {})
+gulp.task('default', gulp.series('minify', function (done) { done() }))
