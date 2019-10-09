525979bd9495c9c653717d12517e9c3126a14a1d
Chandra Prakash <chandra.pr.158@gmail.com>
2019-10-09 22:41:42 +0800

    Fix lint

--- fingerprint2.js
+++ fingerprint2.js
@@ -1278,8 +1278,8 @@
     return gl
   }
   var loseWebglContext = function (context) {
-    var loseContextExtension = context.getExtension("WEBGL_lose_context")
-    if(loseContextExtension != null) {
+    var loseContextExtension = context.getExtension('WEBGL_lose_context')
+    if (loseContextExtension != null) {
       loseContextExtension.loseContext()
     }
   }
