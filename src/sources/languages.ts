375cd08167fddbf8f2da50e81f6f34da6b3489ee
Surgie Finesse <finesserus@gmail.com>
2020-10-13 08:11:41 +1000

    Make a workaround for the languages component in Chrome 86 (#562)
    * Ignore navigator.languages in Chrome because a different behavior in regular and Incognito modes
    
    * Minor amendments and typo fixes

--- src/sources/languages.ts
+++ src/sources/languages.ts
@@ -1,3 +1,5 @@
+import { isChromium, isChromium86OrNewer } from '../utils/browser'
+
 const n = navigator
 
 export default function getLanguages(): string[][] {
@@ -9,7 +11,11 @@ export default function getLanguages(): string[][] {
   }
 
   if (Array.isArray(n.languages)) {
-    result.push(n.languages)
+    // Starting from Chromium 86, there is only a single value in `navigator.language` in Incognito mode:
+    // the value of `navigator.language`. Therefore the value is ignored in this browser.
+    if (!(isChromium() && isChromium86OrNewer())) {
+      result.push(n.languages)
+    }
   } else if (typeof n.languages === 'string') {
     const languages = n.languages as string
     if (languages) {
