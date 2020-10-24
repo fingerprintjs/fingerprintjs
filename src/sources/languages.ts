600bb237dae9800d400178edf5db4201266a32f6
Emmanuel Haguet <Manu1400@users.noreply.github.com>
2020-10-24 22:58:28 +0200

    [typescript] avoid error in future Typescript 4.1
    see https://github.com/microsoft/TypeScript/issues/41155

--- src/sources/languages.ts
+++ src/sources/languages.ts
@@ -14,7 +14,7 @@ export default function getLanguages(): string[][] {
     // Starting from Chromium 86, there is only a single value in `navigator.language` in Incognito mode:
     // the value of `navigator.language`. Therefore the value is ignored in this browser.
     if (!(isChromium() && isChromium86OrNewer())) {
-      result.push(n.languages)
+      result.push(n.languages as string[])
     }
   } else if (typeof n.languages === 'string') {
     const languages = n.languages as string
