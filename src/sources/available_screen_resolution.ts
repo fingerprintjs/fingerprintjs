5d988c4a346cc933a33b30ffb9e56f7d97bf952f
Surgie Finesse <finesserus@gmail.com>
2020-11-16 17:58:38 +1000

    Leave the available screen resolution component only in browsers where it's stable

--- src/sources/available_screen_resolution.ts
+++ src/sources/available_screen_resolution.ts
@@ -1,14 +1,32 @@
 import { toInt } from '../utils/data'
-
-const w = window
+import * as browser from '../utils/browser'
 
 export default function getAvailableScreenResolution(): [number, number] | undefined {
-  if (w.screen.availWidth && w.screen.availHeight) {
-    // Some browsers return screen resolution as strings, e.g. "1200", instead of a number, e.g. 1200.
-    // I suspect it's done by certain plugins that randomize browser properties to prevent fingerprinting.
-    const dimensions = [toInt(w.screen.availWidth), toInt(w.screen.availHeight)] as [number, number]
-    dimensions.sort().reverse()
-    return dimensions
+  const { availWidth, availHeight } = screen
+
+  if (!availWidth || !availHeight || !isStable()) {
+    return undefined
   }
-  return undefined
+
+  // Some browsers return screen resolution as strings, e.g. "1200", instead of a number, e.g. 1200.
+  // I suspect it's done by certain plugins that randomize browser properties to prevent fingerprinting.
+  const dimensions = [toInt(availWidth), toInt(availHeight)] as [number, number]
+  dimensions.sort().reverse()
+  return dimensions
+}
+
+/**
+ * Checks whether the current browser is know to have stable available screen resolution. "Stable" means that it doesn't
+ * change when the browser goes fullscreen (including via `requestFullscreen` and a UI button).
+ *
+ * @see https://github.com/fingerprintjs/fingerprintjs/issues/568#issuecomment-722272726 Based on research in November 2020
+ */
+function isStable() {
+  return (
+    (browser.isWindows() && ((browser.isChromium() && !browser.isChromium84OrNewer()) || browser.isGecko())) ||
+    browser.isTrident() ||
+    browser.isEdgeHTML() ||
+    (browser.isWebKit() && browser.isDesktopSafari() && !browser.isWebKit606OrNewer()) ||
+    (browser.isMacOS() && browser.isChromium() && browser.isChromium57OrNewer() && !browser.isChromium79OrNewer())
+  )
 }
