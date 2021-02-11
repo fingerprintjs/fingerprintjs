058ab8b9a568360004adc5424a492294169c2535
Surgie Finesse <finesserus@gmail.com>
2021-02-11 12:16:17 +1000

    Fix the WebGL error in IE10

--- fingerprint2.js
+++ fingerprint2.js
@@ -991,109 +991,115 @@
 
     gl = getWebglCanvas()
     if (!gl) { return null }
-    // WebGL fingerprinting is a combination of techniques, found in MaxMind antifraud script & Augur fingerprinting.
-    // First it draws a gradient object with shaders and convers the image to the Base64 string.
-    // Then it enumerates all WebGL extensions & capabilities and appends them to the Base64 string, resulting in a huge WebGL string, potentially very unique on each device
-    // Since iOS supports webgl starting from version 8.1 and 8.1 runs on several graphics chips, the results may be different across ios devices, but we need to verify it.
-    var result = []
-    var vShaderTemplate = 'attribute vec2 attrVertex;varying vec2 varyinTexCoordinate;uniform vec2 uniformOffset;void main(){varyinTexCoordinate=attrVertex+uniformOffset;gl_Position=vec4(attrVertex,0,1);}'
-    var fShaderTemplate = 'precision mediump float;varying vec2 varyinTexCoordinate;void main() {gl_FragColor=vec4(varyinTexCoordinate,0,1);}'
-    var vertexPosBuffer = gl.createBuffer()
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexPosBuffer)
-    var vertices = new Float32Array([-0.2, -0.9, 0, 0.4, -0.26, 0, 0, 0.732134444, 0])
-    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW)
-    vertexPosBuffer.itemSize = 3
-    vertexPosBuffer.numItems = 3
-    var program = gl.createProgram()
-    var vshader = gl.createShader(gl.VERTEX_SHADER)
-    gl.shaderSource(vshader, vShaderTemplate)
-    gl.compileShader(vshader)
-    var fshader = gl.createShader(gl.FRAGMENT_SHADER)
-    gl.shaderSource(fshader, fShaderTemplate)
-    gl.compileShader(fshader)
-    gl.attachShader(program, vshader)
-    gl.attachShader(program, fshader)
-    gl.linkProgram(program)
-    gl.useProgram(program)
-    program.vertexPosAttrib = gl.getAttribLocation(program, 'attrVertex')
-    program.offsetUniform = gl.getUniformLocation(program, 'uniformOffset')
-    gl.enableVertexAttribArray(program.vertexPosArray)
-    gl.vertexAttribPointer(program.vertexPosAttrib, vertexPosBuffer.itemSize, gl.FLOAT, !1, 0, 0)
-    gl.uniform2f(program.offsetUniform, 1, 1)
-    gl.drawArrays(gl.TRIANGLE_STRIP, 0, vertexPosBuffer.numItems)
     try {
-      result.push(gl.canvas.toDataURL())
-    } catch (e) {
-      /* .toDataURL may be absent or broken (blocked by extension) */
-    }
-    result.push('extensions:' + (gl.getSupportedExtensions() || []).join(';'))
-    result.push('webgl aliased line width range:' + fa2s(gl.getParameter(gl.ALIASED_LINE_WIDTH_RANGE)))
-    result.push('webgl aliased point size range:' + fa2s(gl.getParameter(gl.ALIASED_POINT_SIZE_RANGE)))
-    result.push('webgl alpha bits:' + gl.getParameter(gl.ALPHA_BITS))
-    result.push('webgl antialiasing:' + (gl.getContextAttributes().antialias ? 'yes' : 'no'))
-    result.push('webgl blue bits:' + gl.getParameter(gl.BLUE_BITS))
-    result.push('webgl depth bits:' + gl.getParameter(gl.DEPTH_BITS))
-    result.push('webgl green bits:' + gl.getParameter(gl.GREEN_BITS))
-    result.push('webgl max anisotropy:' + maxAnisotropy(gl))
-    result.push('webgl max combined texture image units:' + gl.getParameter(gl.MAX_COMBINED_TEXTURE_IMAGE_UNITS))
-    result.push('webgl max cube map texture size:' + gl.getParameter(gl.MAX_CUBE_MAP_TEXTURE_SIZE))
-    result.push('webgl max fragment uniform vectors:' + gl.getParameter(gl.MAX_FRAGMENT_UNIFORM_VECTORS))
-    result.push('webgl max render buffer size:' + gl.getParameter(gl.MAX_RENDERBUFFER_SIZE))
-    result.push('webgl max texture image units:' + gl.getParameter(gl.MAX_TEXTURE_IMAGE_UNITS))
-    result.push('webgl max texture size:' + gl.getParameter(gl.MAX_TEXTURE_SIZE))
-    result.push('webgl max varying vectors:' + gl.getParameter(gl.MAX_VARYING_VECTORS))
-    result.push('webgl max vertex attribs:' + gl.getParameter(gl.MAX_VERTEX_ATTRIBS))
-    result.push('webgl max vertex texture image units:' + gl.getParameter(gl.MAX_VERTEX_TEXTURE_IMAGE_UNITS))
-    result.push('webgl max vertex uniform vectors:' + gl.getParameter(gl.MAX_VERTEX_UNIFORM_VECTORS))
-    result.push('webgl max viewport dims:' + fa2s(gl.getParameter(gl.MAX_VIEWPORT_DIMS)))
-    result.push('webgl red bits:' + gl.getParameter(gl.RED_BITS))
-    result.push('webgl renderer:' + gl.getParameter(gl.RENDERER))
-    result.push('webgl shading language version:' + gl.getParameter(gl.SHADING_LANGUAGE_VERSION))
-    result.push('webgl stencil bits:' + gl.getParameter(gl.STENCIL_BITS))
-    result.push('webgl vendor:' + gl.getParameter(gl.VENDOR))
-    result.push('webgl version:' + gl.getParameter(gl.VERSION))
-
-    try {
-      // Add the unmasked vendor and unmasked renderer if the debug_renderer_info extension is available
-      var extensionDebugRendererInfo = gl.getExtension('WEBGL_debug_renderer_info')
-      if (extensionDebugRendererInfo) {
-        result.push('webgl unmasked vendor:' + gl.getParameter(extensionDebugRendererInfo.UNMASKED_VENDOR_WEBGL))
-        result.push('webgl unmasked renderer:' + gl.getParameter(extensionDebugRendererInfo.UNMASKED_RENDERER_WEBGL))
+      // WebGL fingerprinting is a combination of techniques, found in MaxMind antifraud script & Augur fingerprinting.
+      // First it draws a gradient object with shaders and convers the image to the Base64 string.
+      // Then it enumerates all WebGL extensions & capabilities and appends them to the Base64 string, resulting in a huge WebGL string, potentially very unique on each device
+      // Since iOS supports webgl starting from version 8.1 and 8.1 runs on several graphics chips, the results may be different across ios devices, but we need to verify it.
+      var result = []
+      var vShaderTemplate = 'attribute vec2 attrVertex;varying vec2 varyinTexCoordinate;uniform vec2 uniformOffset;void main(){varyinTexCoordinate=attrVertex+uniformOffset;gl_Position=vec4(attrVertex,0,1);}'
+      var fShaderTemplate = 'precision mediump float;varying vec2 varyinTexCoordinate;void main() {gl_FragColor=vec4(varyinTexCoordinate,0,1);}'
+      var vertexPosBuffer = gl.createBuffer()
+      gl.bindBuffer(gl.ARRAY_BUFFER, vertexPosBuffer)
+      var vertices = new Float32Array([-0.2, -0.9, 0, 0.4, -0.26, 0, 0, 0.732134444, 0])
+      gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW)
+      vertexPosBuffer.itemSize = 3
+      vertexPosBuffer.numItems = 3
+      var program = gl.createProgram()
+      var vshader = gl.createShader(gl.VERTEX_SHADER)
+      gl.shaderSource(vshader, vShaderTemplate)
+      gl.compileShader(vshader)
+      var fshader = gl.createShader(gl.FRAGMENT_SHADER)
+      gl.shaderSource(fshader, fShaderTemplate)
+      gl.compileShader(fshader)
+      gl.attachShader(program, vshader)
+      gl.attachShader(program, fshader)
+      gl.linkProgram(program)
+      gl.useProgram(program)
+      program.vertexPosAttrib = gl.getAttribLocation(program, 'attrVertex')
+      program.offsetUniform = gl.getUniformLocation(program, 'uniformOffset')
+      gl.enableVertexAttribArray(program.vertexPosArray)
+      gl.vertexAttribPointer(program.vertexPosAttrib, vertexPosBuffer.itemSize, gl.FLOAT, !1, 0, 0)
+      gl.uniform2f(program.offsetUniform, 1, 1)
+      gl.drawArrays(gl.TRIANGLE_STRIP, 0, vertexPosBuffer.numItems)
+      try {
+        result.push(gl.canvas.toDataURL())
+      } catch (e) {
+        /* .toDataURL may be absent or broken (blocked by extension) */
       }
-    } catch (e) { /* squelch */ }
+      result.push('extensions:' + (gl.getSupportedExtensions() || []).join(';'))
+      result.push('webgl aliased line width range:' + fa2s(gl.getParameter(gl.ALIASED_LINE_WIDTH_RANGE)))
+      result.push('webgl aliased point size range:' + fa2s(gl.getParameter(gl.ALIASED_POINT_SIZE_RANGE)))
+      result.push('webgl alpha bits:' + gl.getParameter(gl.ALPHA_BITS))
+      result.push('webgl antialiasing:' + (gl.getContextAttributes().antialias ? 'yes' : 'no'))
+      result.push('webgl blue bits:' + gl.getParameter(gl.BLUE_BITS))
+      result.push('webgl depth bits:' + gl.getParameter(gl.DEPTH_BITS))
+      result.push('webgl green bits:' + gl.getParameter(gl.GREEN_BITS))
+      result.push('webgl max anisotropy:' + maxAnisotropy(gl))
+      result.push('webgl max combined texture image units:' + gl.getParameter(gl.MAX_COMBINED_TEXTURE_IMAGE_UNITS))
+      result.push('webgl max cube map texture size:' + gl.getParameter(gl.MAX_CUBE_MAP_TEXTURE_SIZE))
+      result.push('webgl max fragment uniform vectors:' + gl.getParameter(gl.MAX_FRAGMENT_UNIFORM_VECTORS))
+      result.push('webgl max render buffer size:' + gl.getParameter(gl.MAX_RENDERBUFFER_SIZE))
+      result.push('webgl max texture image units:' + gl.getParameter(gl.MAX_TEXTURE_IMAGE_UNITS))
+      result.push('webgl max texture size:' + gl.getParameter(gl.MAX_TEXTURE_SIZE))
+      result.push('webgl max varying vectors:' + gl.getParameter(gl.MAX_VARYING_VECTORS))
+      result.push('webgl max vertex attribs:' + gl.getParameter(gl.MAX_VERTEX_ATTRIBS))
+      result.push('webgl max vertex texture image units:' + gl.getParameter(gl.MAX_VERTEX_TEXTURE_IMAGE_UNITS))
+      result.push('webgl max vertex uniform vectors:' + gl.getParameter(gl.MAX_VERTEX_UNIFORM_VECTORS))
+      result.push('webgl max viewport dims:' + fa2s(gl.getParameter(gl.MAX_VIEWPORT_DIMS)))
+      result.push('webgl red bits:' + gl.getParameter(gl.RED_BITS))
+      result.push('webgl renderer:' + gl.getParameter(gl.RENDERER))
+      result.push('webgl shading language version:' + gl.getParameter(gl.SHADING_LANGUAGE_VERSION))
+      result.push('webgl stencil bits:' + gl.getParameter(gl.STENCIL_BITS))
+      result.push('webgl vendor:' + gl.getParameter(gl.VENDOR))
+      result.push('webgl version:' + gl.getParameter(gl.VERSION))
 
-    if (!gl.getShaderPrecisionFormat) {
-      loseWebglContext(gl)
-      return result
-    }
+      try {
+        // Add the unmasked vendor and unmasked renderer if the debug_renderer_info extension is available
+        var extensionDebugRendererInfo = gl.getExtension('WEBGL_debug_renderer_info')
+        if (extensionDebugRendererInfo) {
+          result.push('webgl unmasked vendor:' + gl.getParameter(extensionDebugRendererInfo.UNMASKED_VENDOR_WEBGL))
+          result.push('webgl unmasked renderer:' + gl.getParameter(extensionDebugRendererInfo.UNMASKED_RENDERER_WEBGL))
+        }
+      } catch (e) { /* squelch */ }
 
-    each(['FLOAT', 'INT'], function (numType) {
-      each(['VERTEX', 'FRAGMENT'], function (shader) {
-        each(['HIGH', 'MEDIUM', 'LOW'], function (numSize) {
-          each(['precision', 'rangeMin', 'rangeMax'], function (key) {
-            var format = gl.getShaderPrecisionFormat(gl[shader + '_SHADER'], gl[numSize + '_' + numType])[key]
-            if (key !== 'precision') {
-              key = 'precision ' + key
-            }
-            var line = ['webgl ', shader.toLowerCase(), ' shader ', numSize.toLowerCase(), ' ', numType.toLowerCase(), ' ', key, ':', format].join('')
-            result.push(line)
+      if (!gl.getShaderPrecisionFormat) {
+        return result
+      }
+
+      each(['FLOAT', 'INT'], function (numType) {
+        each(['VERTEX', 'FRAGMENT'], function (shader) {
+          each(['HIGH', 'MEDIUM', 'LOW'], function (numSize) {
+            each(['precision', 'rangeMin', 'rangeMax'], function (key) {
+              var format = gl.getShaderPrecisionFormat(gl[shader + '_SHADER'], gl[numSize + '_' + numType])[key]
+              if (key !== 'precision') {
+                key = 'precision ' + key
+              }
+              var line = ['webgl ', shader.toLowerCase(), ' shader ', numSize.toLowerCase(), ' ', numType.toLowerCase(), ' ', key, ':', format].join('')
+              result.push(line)
+            })
           })
         })
       })
-    })
-    loseWebglContext(gl)
-    return result
+      return result
+    } finally {
+      try {
+        loseWebglContext(gl)
+      } catch (e) { /* Let the original error be thrown */ }
+    }
   }
   var getWebglVendorAndRenderer = function () {
     /* This a subset of the WebGL fingerprint with a lot of entropy, while being reasonably browser-independent */
     try {
       var glContext = getWebglCanvas()
       var extensionDebugRendererInfo = glContext.getExtension('WEBGL_debug_renderer_info')
-      var params = glContext.getParameter(extensionDebugRendererInfo.UNMASKED_VENDOR_WEBGL) + '~' + glContext.getParameter(extensionDebugRendererInfo.UNMASKED_RENDERER_WEBGL)
-      loseWebglContext(glContext)
-      return params
+      return glContext.getParameter(extensionDebugRendererInfo.UNMASKED_VENDOR_WEBGL) + '~' + glContext.getParameter(extensionDebugRendererInfo.UNMASKED_RENDERER_WEBGL)
     } catch (e) {
       return null
+    } finally {
+      try {
+        loseWebglContext(glContext)
+      } catch (e) { /* Keep the original result */ }
     }
   }
   var getAdBlock = function () {
@@ -1263,14 +1269,19 @@
   }
   var isWebGlSupported = function () {
     // code taken from Modernizr
-    if (!isCanvasSupported()) {
+    if (!isCanvasSupported() || !window.WebGLRenderingContext) {
       return false
     }
 
     var glContext = getWebglCanvas()
-    var isSupported = !!window.WebGLRenderingContext && !!glContext
-    loseWebglContext(glContext)
-    return isSupported
+    if (glContext) {
+      try {
+        loseWebglContext(glContext)
+      } catch (e) { /* The try block is optional, so let the main algorithm continue */ }
+      return true
+    } else {
+      return false
+    }
   }
   var isIE = function () {
     if (navigator.appName === 'Microsoft Internet Explorer') {
