e073bbd8dc95a9005160acc6ded41026b2fc84cb
Surgie Finesse <finesserus@gmail.com>
2021-03-02 19:31:07 +1000

    Make the canvas be encoding independent; reduce the canvas code size

--- src/sources/canvas.ts
+++ src/sources/canvas.ts
@@ -31,8 +31,8 @@ export default function getCanvasFingerprint(): CanvasFingerprint {
     return { winding: false, data: '' }
   }
 
-  // detect browser support of canvas winding
-  // http://blogs.adobe.com/webplatform/2013/01/30/winding-rules-in-canvas/
+  // Detect browser support of canvas winding
+  // https://web.archive.org/web/20170825024655/http://blogs.adobe.com/webplatform/2013/01/30/winding-rules-in-canvas/
   // https://github.com/Modernizr/Modernizr/blob/master/feature-detects/canvas/winding.js
   context.rect(0, 0, 10, 10)
   context.rect(2, 2, 6, 6)
@@ -42,41 +42,40 @@ export default function getCanvasFingerprint(): CanvasFingerprint {
   context.fillStyle = '#f60'
   context.fillRect(125, 1, 62, 20)
   context.fillStyle = '#069'
-  // https://github.com/Valve/fingerprintjs2/issues/66
-  // this can affect FP generation when applying different CSS on different websites
+  // This can affect FP generation when applying different CSS on different websites:
+  // https://github.com/fingerprintjs/fingerprintjs/issues/66
   context.font = '11pt no-real-font-123'
-  // the choice of emojis has a gigantic impact on rendering performance (especially in FF)
-  // some newer emojis cause it to slow down 50-200 times
-  // context.fillText("Cw爨m fjordbank \ud83d\ude03 gly", 2, 15)
-  const printedText = 'Cwm fjordbank \ud83d\ude03 gly'
+  // The choice of emojis has a gigantic impact on rendering performance (especially in FF).
+  // Some newer emojis cause it to slow down 50-200 times.
+  // A bare emoji shouldn't be used because the canvas will change depending on the script encoding:
+  // https://github.com/fingerprintjs/fingerprintjs/issues/66
+  // Escape sequence shouldn't be used too because Terser will turn it into a bare unicode.
+  const printedText = `Cwm fjordbank ${String.fromCharCode(55357, 56835) /* 😃 */} gly`
   context.fillText(printedText, 2, 15)
   context.fillStyle = 'rgba(102, 204, 0, 0.2)'
   context.font = '18pt Arial'
   context.fillText(printedText, 4, 45)
 
-  // canvas blending
-  // http://blogs.adobe.com/webplatform/2013/01/28/blending-features-in-canvas/
+  // Canvas blending
+  // https://web.archive.org/web/20170826194121/http://blogs.adobe.com/webplatform/2013/01/28/blending-features-in-canvas/
   // http://jsfiddle.net/NDYV8/16/
   context.globalCompositeOperation = 'multiply'
-  context.fillStyle = 'rgb(255,0,255)'
-  context.beginPath()
-  context.arc(50, 50, 50, 0, Math.PI * 2, true)
-  context.closePath()
-  context.fill()
-  context.fillStyle = 'rgb(0,255,255)'
-  context.beginPath()
-  context.arc(100, 50, 50, 0, Math.PI * 2, true)
-  context.closePath()
-  context.fill()
-  context.fillStyle = 'rgb(255,255,0)'
-  context.beginPath()
-  context.arc(75, 100, 50, 0, Math.PI * 2, true)
-  context.closePath()
-  context.fill()
-  context.fillStyle = 'rgb(255,0,255)'
-  // canvas winding
+  for (const [color, x, y] of [
+    ['#f0f', 50, 50],
+    ['#0ff', 100, 50],
+    ['#ff0', 75, 100],
+  ] as const) {
+    context.fillStyle = color
+    context.beginPath()
+    context.arc(x, y, 50, 0, Math.PI * 2, true)
+    context.closePath()
+    context.fill()
+  }
+
+  // Canvas winding
   // http://blogs.adobe.com/webplatform/2013/01/30/winding-rules-in-canvas/
   // http://jsfiddle.net/NDYV8/19/
+  context.fillStyle = '#f0f'
   context.arc(75, 75, 75, 0, Math.PI * 2, true)
   context.arc(75, 75, 25, 0, Math.PI * 2, true)
   context.fill('evenodd')
