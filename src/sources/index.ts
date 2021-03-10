b0489a4ed520d1375cec92f80a517e0b6c57054f
Surgie Finesse <finesserus@gmail.com>
2021-03-10 17:09:12 +1000

    Try to let asynchronous sources complete between other signals

--- src/sources/index.ts
+++ src/sources/index.ts
@@ -160,30 +160,21 @@ export async function getComponents<
   sourceOptions: TSourceOptions,
   excludeSources: readonly TExclude[],
 ): Promise<Omit<SourcesToComponents<TSources>, TExclude>> {
-  const sourcePromises: Promise<unknown>[] = []
   const components = {} as Omit<SourcesToComponents<TSources>, TExclude>
+  let componentCounter = 0
 
-  // Create the keys immediately to keep the component keys order the same as the sources keys order
-  for (const sourceKey of Object.keys(sources) as Array<keyof TSources>) {
-    if (excludes(excludeSources, sourceKey)) {
-      components[sourceKey] = undefined as any
-    }
-  }
+  await Promise.all(
+    Object.keys(sources).map(async (sourceKey: keyof TSources) => {
+      if (excludes(excludeSources, sourceKey)) {
+        // Create the keys immediately to keep the component keys order the same as the sources keys order
+        components[sourceKey] = undefined as any
+        await new Promise((resolve) => setTimeout(resolve, componentCounter++))
+        const component = await getComponent(sources[sourceKey], sourceOptions)
+        components[sourceKey] = component as Component<any>
+      }
+    }),
+  )
 
-  for (const sourceKey of Object.keys(components) as Array<keyof typeof components>) {
-    sourcePromises.push(
-      getComponent(sources[sourceKey], sourceOptions).then((component: Component<any>) => {
-        components[sourceKey] = component
-      }),
-    )
-
-    // It breaks the synchronous flow. That serves several goals:
-    // - Makes `getComponent` measure duration of synchronous sources without including other source operations;
-    // - Allows asynchronous sources to complete before all the sources are started;
-    await undefined
-  }
-
-  await Promise.all(sourcePromises)
   return components
 }
 
