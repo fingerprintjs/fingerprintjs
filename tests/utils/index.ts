07f581abe71f0636a51714d49b92493a18785bdb
Surgie Finesse <finesserus@gmail.com>
2021-04-13 22:33:50 +1000

    Clean-up

--- tests/utils/index.ts
+++ tests/utils/index.ts
@@ -8,7 +8,7 @@ export { default as withMockMatchMedia } from './mock_match_media'
  * Karma doesn't provide this information.
  * The browser detect functions in the `src` directory can't be used because they are objects of testing.
  * Therefore a popular third party library is used to detect browser.
- * The library isn't used in the main code because some visitors tamper user agent while test the environments don't.
+ * The library isn't used in the main code because some visitors tamper user agent while the test environments don't.
  *
  * We should find a way to pass browser settings from the Karma configuration to the tests,
  * otherwise we can't distinguish incognito browsers from regular browsers, Brave from Chrome, etc.
