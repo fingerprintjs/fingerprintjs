422c1da1a19ceb2a8a38796624615667a13010cd
Ankur Parihar <pariharankur101@gmail.com>
2021-11-30 11:02:52 +0530

    Fix comment for font detection (#715)

--- src/sources/fonts.ts
+++ src/sources/fonts.ts
@@ -8,7 +8,7 @@ const testString = 'mmMwWLliI0O&1'
 const textSize = '48px'
 
 // A font will be compared against all the three default fonts.
-// And if it doesn't match all 3 then that font is not available.
+// And if for any default fonts it doesn't match, then that font is available.
 const baseFonts = ['monospace', 'sans-serif', 'serif'] as const
 
 const fontList = [
