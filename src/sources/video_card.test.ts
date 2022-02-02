245ec4573f4707f1f8de9ca4c838d1f4e922382f
webitral <eh.nasreddine@gmail.com>
2022-02-02 20:10:14 +0100

    importing isGecko from ../../tests/utils

--- src/sources/video_card.test.ts
+++ src/sources/video_card.test.ts
@@ -1,18 +1,15 @@
 import getVideoCard from './video_card'
-import { isGecko } from '../utils/browser'
-import { getBrowserMajorVersion } from '../../tests/utils'
+import { isGecko, getBrowserMajorVersion } from '../../tests/utils'
 
 describe('Sources', () => {
   describe('videoCard', () => {
-    const videoCard = getVideoCard()
+    it('to be correctly typed if defined', () => {
+      const videoCard = getVideoCard()
 
-    it('to be defined', () => {
       if (!isGecko() || (getBrowserMajorVersion() ?? Infinity) >= 53) {
         expect(videoCard).toBeTruthy()
       }
-    })
 
-    it('to be correctly typed if defined', () => {
       if (videoCard) {
         expect(videoCard).toEqual({
           vendor: jasmine.any(String),
