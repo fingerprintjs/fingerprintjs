54542706feb167d8e17edf3c5aa181c2c251072f
webitral <eh.nasreddine@gmail.com>
2022-02-02 18:14:59 +0100

    Added to be defined test for video_card entropy source.

--- src/sources/video_card.test.ts
+++ src/sources/video_card.test.ts
@@ -1,10 +1,18 @@
 import getVideoCard from './video_card'
+import { isGecko } from '../utils/browser'
+import { getBrowserMajorVersion } from '../../tests/utils'
 
 describe('Sources', () => {
   describe('videoCard', () => {
-    it('to be correctly typed if defined', () => {
-      const videoCard = getVideoCard()
+    const videoCard = getVideoCard()
 
+    it('to be defined', () => {
+      if (!isGecko() || (getBrowserMajorVersion() ?? Infinity) >= 53) {
+        expect(videoCard).toBeTruthy()
+      }
+    })
+
+    it('to be correctly typed if defined', () => {
       if (videoCard) {
         expect(videoCard).toEqual({
           vendor: jasmine.any(String),
