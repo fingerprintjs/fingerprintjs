417efc5027ec48bbf975528c0193de3df90b4ef1
kalexey89 <kyznecov.alexey@gmail.com>
2022-06-22 02:04:22 +0300

    feat(metamask): add metamask detection

---
+++ src/sources/metamask.ts
@@ -0,0 +1,8 @@
+import { isBrave } from '../utils/browser';
+
+/**
+ * Detects if the {@link https://metamask.io/ MetaMask} extension is installed
+ */
+export default function isMetaMaskInstalled(): boolean {
+  return !!(typeof window.ethereum !== 'undefined' && !isBrave());
+}
