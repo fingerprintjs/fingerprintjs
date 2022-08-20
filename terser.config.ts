2e59a0378b0c2b368d1344e009553cfcf25b4c08
Dmitry Ivakhnenko <jeetiss@yandex.ru>
2022-08-20 15:26:43 +0300

    run terser twice

--- terser.config.ts
+++ terser.config.ts
@@ -1,6 +1,9 @@
 // Note: this file is non standard, it's imported explicitly
 // Reference: https://github.com/terser/terser
 export default {
+  compress: {
+    passes: 2,
+  },
   format: {
     comments: false,
   },
