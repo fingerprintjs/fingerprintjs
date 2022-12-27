e4d22de0ab249c5c05ce079f9a0d16d21349faaf
Sergey M <finesserus@gmail.com>
2022-12-27 22:07:43 +0400

    Remove IE-specific test conditions

--- src/sources/audio.test.ts
+++ src/sources/audio.test.ts
@@ -1,4 +1,4 @@
-import { getBrowserMajorVersion, isMobile, isSafari, isTrident } from '../../tests/utils'
+import { getBrowserMajorVersion, isMobile, isSafari } from '../../tests/utils'
 import getAudioFingerprint, { SpecialFingerprint } from './audio'
 
 describe('Sources', () => {
@@ -6,9 +6,7 @@ describe('Sources', () => {
     it('returns expected value type depending on the browser', async () => {
       const result = getAudioFingerprint()
 
-      if (isTrident()) {
-        expect(result).toBe(SpecialFingerprint.NotSupported)
-      } else if (isSafari() && isMobile() && (getBrowserMajorVersion() ?? 0) < 12) {
+      if (isSafari() && isMobile() && (getBrowserMajorVersion() ?? 0) < 12) {
         // WebKit has stopped telling its real version in the user-agent string since version 605.1.15,
         // therefore the browser version has to be checked instead of the engine version.
         expect(result).toBe(SpecialFingerprint.KnownToSuspend)
