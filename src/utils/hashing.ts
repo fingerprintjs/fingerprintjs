c3f2ccaea843e607734fba1c81b3e18239913b49
Ilya Kuznetsov <ilia.kuznetsov@fingerprint.com>
2023-07-30 14:34:43 +0400

    docs: Benchmark link

--- src/utils/hashing.ts
+++ src/utils/hashing.ts
@@ -154,6 +154,9 @@ const N2 = [0, 0x38495ab5]
 /**
  * Given a string and an optional seed as an int, returns a 128 bit
  * hash using the x64 flavor of MurmurHash3, as an unsigned hex.
+ * All internal functions mutates passed value to achieve minimal memory allocations and GC load
+ *
+ * Benchmark https://jsbench.me/p4lkpaoabi/1
  */
 export function x64hash128(input: string, seed?: number): string {
   const key = getUTF8Bytes(input)
