703ea109f61bb6dbec98d0fa69d927c7d04024a4
Martin Bajanik <bayotop@users.noreply.github.com>
2024-05-30 11:04:09 +0200

    Update ApplePay exports (#1005)

--- src/sources/apple_pay.ts
+++ src/sources/apple_pay.ts
@@ -14,7 +14,11 @@ export const enum ApplePayState {
   NotAvailableInFrame = -3,
 }
 
-export default function getApplePayState(): ApplePayState {
+/**
+ * The return type is a union instead of the enum, because it's too challenging to embed the const enum into another
+ * project. Turning it into a union is a simple and an elegant solution.
+ */
+export default function getApplePayState(): 0 | 1 | -1 | -2 | -3 {
   const { ApplePaySession } = window
 
   if (typeof ApplePaySession?.canMakePayments !== 'function') {
@@ -28,14 +32,9 @@ export default function getApplePayState(): ApplePayState {
   }
 }
 
-/**
- * The return type is a union instead of the enum, because it's too challenging to embed the const enum into another
- * project. Turning it into a union is a simple and an elegant solution.
- *
- * Warning for package users:
- * This function is out of Semantic Versioning, i.e. can change unexpectedly. Usage is at your own risk.
- */
-export function getStateFromError(error: unknown): -2 | -3 {
+export function getStateFromError(
+  error: unknown,
+): Exclude<ApplePayState, ApplePayState.Disabled | ApplePayState.Enabled | ApplePayState.NoAPI> {
   if (error instanceof Error) {
     // See full expected error messages in the test
     if (error.name === 'InvalidAccessError') {
