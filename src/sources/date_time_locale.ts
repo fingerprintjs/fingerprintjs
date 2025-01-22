9669e8b85eb2ffaca53f826ec5cc74086dd1cb12
M. Jocic <milos.jocic@fingerprint.com>
2025-01-22 14:54:24 +0100

    refacator getDateTimeLocale to avoid explic if clause

--- src/sources/date_time_locale.ts
+++ src/sources/date_time_locale.ts
@@ -2,13 +2,9 @@
  * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat/resolvedOptions
  */
 export default function getDateTimeLocale(): string {
-  const DateTimeFormat = window.Intl?.DateTimeFormat
-  if (DateTimeFormat) {
-    const locale = new DateTimeFormat().resolvedOptions().locale
-    if (locale) {
-      return locale
-    }
-  }
+  return getLocaleFromResolvedOption()
+}
 
-  return ''
+function getLocaleFromResolvedOption(): string {
+  return window.Intl?.DateTimeFormat().resolvedOptions().locale || ''
 }
