b841a579930cb2e0ff8a515652f66f1051807b43
M. Jocic <milos.jocic@fingerprint.com>
2025-01-22 14:00:06 +0100

    fixing test describe

--- src/sources/date_time_locale.test.ts
+++ src/sources/date_time_locale.test.ts
@@ -11,7 +11,7 @@ interface ResolvedDateTimeFormatOptions {
 
 describe('Sources', () => {
   describe('dateTimeLocale', () => {
-    describe("cases for browsers that don't support Intl or locale", () => {
+    describe('cases for browsers that support Intl or locale', () => {
       it('returns string representing dateTime locale', () => {
         const result = getDateTimeLocale()
         expect(typeof result).toBe('string')
