b24c00b893b7dccee4ba241f2593f30d3a3deb0d
M. Jocic <milos.jocic@fingerprint.com>
2025-01-22 12:25:17 +0100

    adding fallback in case locale is not defined or window.Intl is not supported

--- src/sources/date_time_locale.test.ts
+++ src/sources/date_time_locale.test.ts
@@ -1,12 +1,44 @@
 import getDateTimeLocale from './date_time_locale'
 
-describe('Sources', () => {
+fdescribe('Sources', () => {
   describe('dateTimeLocale', () => {
-    it('returns string representing dateTime locale', () => {
-      const result = getDateTimeLocale()
-      expect(typeof result).toBe('string')
-      // The combinations returned as results for the current test suite specified browsers are: "en", "en-US", "en-GB".
-      expect(result).toMatch(/^en(-[A-Z]{2})?$/)
+    describe("cases for browsers that don't support Intl or locale", () => {
+      it('returns string representing dateTime locale', () => {
+        const result = getDateTimeLocale()
+        expect(typeof result).toBe('string')
+        // The combinations returned as results for the current test suite specified browsers are:
+        // "en", "en-US", "en-GB".
+        expect(result).toMatch(/^en(-[A-Z]{2})?$/)
+      })
+    })
+
+    describe("edge cases for browsers that don't support Intl or locale", () => {
+      let originalIntl: typeof Intl
+
+      beforeEach(() => {
+        originalIntl = window.Intl
+      })
+
+      afterEach(() => {
+        window.Intl = originalIntl
+      })
+      it('should return an empty string if Intl.DateTimeFormat is not supported', () => {
+        window.Intl = undefined as any
+
+        const result = getDateTimeLocale()
+        expect(result).toBe('')
+      })
+
+      it('should return an empty string if resolvedOptions().locale is undefined', () => {
+        spyOn(window.Intl, 'DateTimeFormat').and.returnValue({
+          resolvedOptions: () => ({ locale: undefined } as any),
+          format: () => '',
+          formatToParts: () => [],
+        })
+
+        const result = getDateTimeLocale()
+        expect(result).toBe('')
+      })
     })
   })
 })
--- src/sources/date_time_locale.ts
+++ src/sources/date_time_locale.ts
@@ -2,5 +2,13 @@
  * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat/resolvedOptions
  */
 export default function getDateTimeLocale(): string {
-  return window.Intl.DateTimeFormat().resolvedOptions().locale
+  const DateTimeFormat = window.Intl?.DateTimeFormat
+  if (DateTimeFormat) {
+    const locale = new DateTimeFormat().resolvedOptions().locale
+    if (locale) {
+      return locale
+    }
+  }
+
+  return ''
 }
