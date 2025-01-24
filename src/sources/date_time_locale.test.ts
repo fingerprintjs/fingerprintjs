a7001bb22d8e9ef9c8c842eca946901bf43119b2
M. Jocic <milos.jocic@fingerprint.com>
2025-01-24 14:44:56 +0100

    adjusting test logic regarding result check

--- src/sources/date_time_locale.test.ts
+++ src/sources/date_time_locale.test.ts
@@ -6,9 +6,17 @@ describe('Sources', () => {
       it('returns string representing dateTime locale', () => {
         const result = getDateTimeLocale()
         expect(typeof result).toBe('string')
-        // The combinations returned as results for the current test suite specified browsers are:
-        // "en", "en-US", "en-GB".
-        expect(result).toMatch(/^en(-[A-Z]{2})?$/)
+        // The combinations returned as results for the current test suite specified browsers on the BrowserStack are:
+        // "en", "en-US", "en-GB",
+        // However, we need to keep in mind that this is an open-source codebase, and developers who contribute might
+        // be working in local environments that do not fall into this predefined set. Therefore, it is important
+        // that our logic for detecting local test runs is flexible and inclusive, ensuring it works for all
+        // contributors regardless of their setup.
+        // Examples of locales: "en-US", "zh-Hans-CN", "fr-CA", "es-419", regex formed by following->
+        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat/resolvedOptions#:~:text=locale-,The%20BCP%2047,-language%20tag%20for
+        // https://datatracker.ietf.org/doc/html/rfc4647#section-3.4
+        const localeRegex = /^[a-z]{2,3}(-[A-Za-z]{4})?(-([A-Z]{2}|[0-9]{3}))?(-[A-Za-z0-9]+)*$/
+        expect(result).toMatch(localeRegex)
       })
     })
     describe("edge cases for browsers that don't support Intl or locale", () => {
