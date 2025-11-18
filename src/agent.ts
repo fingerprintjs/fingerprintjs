df59b8c98d814caadf66cf07dda9101cbfbaebf6
N. Crnogorac <montenegrini91@gmail.com>
2025-11-18 16:22:12 +0100

    Reverted back the option to disable monitoring and updated the documentation (#1123)
    * Reverted back the option to disable monitoring and updated the documentation
    
    * Added documentation in the v4 to v5 migration doc

--- src/agent.ts
+++ src/agent.ts
@@ -21,6 +21,11 @@ export interface LoadOptions {
    * Required to ease investigations of problems.
    */
   debug?: boolean
+  /**
+   * Set `false` to disable the unpersonalized AJAX request that the agent sends to collect installation statistics.
+   * It's always disabled in the version published to the FingerprintJS CDN.
+   */
+  monitoring?: boolean
 }
 
 /**
@@ -196,10 +201,10 @@ function monitor() {
  * Builds an instance of Agent and waits a delay required for a proper operation.
  */
 export async function load(options: Readonly<LoadOptions> = {}): Promise<Agent> {
-  if ((options as { monitoring?: boolean }).monitoring ?? true) {
+  const { delayFallback, debug, monitoring = true } = options
+  if (monitoring) {
     monitor()
   }
-  const { delayFallback, debug } = options
   await prepareForSources(delayFallback)
   const getComponents = loadBuiltinSources({ cache: {}, debug })
   return makeAgent(getComponents, debug)
