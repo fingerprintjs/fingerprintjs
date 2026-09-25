c3c53744317eb3f60607ff2fcf7a3973ac1ad8c0
Ritesh Kumar <github.riteshkumar@gmail.com>
2026-09-25 16:54:01 +0530

    fix(hdr): skip dynamic-range on mobile WebKit due to Low Power Mode

--- src/sources/hdr.test.ts
+++ src/sources/hdr.test.ts
@@ -1,4 +1,4 @@
-import { withMockMatchMedia } from '../../tests/utils'
+import { isMobile, isTablet, isWebKit, withMockMatchMedia } from '../../tests/utils'
 
 import isHDR from './hdr'
 
@@ -9,6 +9,13 @@ describe('Sources', () => {
     })
 
     it('handles various cases', async () => {
+      // Mobile WebKit is excluded because the value changes in Low Power Mode
+      if (isWebKit() && (isMobile() || isTablet())) {
+        await withMockMatchMedia({ 'dynamic-range': ['high'] }, true, () => expect(isHDR()).toBeUndefined())
+        await withMockMatchMedia({ 'dynamic-range': ['standard'] }, true, () => expect(isHDR()).toBeUndefined())
+        return
+      }
+
       await withMockMatchMedia({ 'dynamic-range': [undefined] }, true, () => expect(isHDR()).toBeUndefined())
       await withMockMatchMedia({ 'dynamic-range': ['high'] }, true, () => expect(isHDR()).toBeTrue())
       await withMockMatchMedia({ 'dynamic-range': ['standard'] }, true, () => expect(isHDR()).toBeFalse())
--- src/sources/hdr.ts
+++ src/sources/hdr.ts
@@ -1,7 +1,14 @@
+import { isDesktopWebKit, isWebKit } from '../utils/browser'
+
 /**
  * @see https://www.w3.org/TR/mediaqueries-5/#dynamic-range
  */
 export default function isHDR(): boolean | undefined {
+  // Mobile WebKit (iOS/iPadOS) reports `standard` dynamic range while Low Power Mode is on, so the value is unstable.
+  // See https://github.com/fingerprintjs/fingerprintjs/issues/809
+  if (isWebKit() && !isDesktopWebKit()) {
+    return undefined
+  }
   if (doesMatch('high')) {
     return true
   }
